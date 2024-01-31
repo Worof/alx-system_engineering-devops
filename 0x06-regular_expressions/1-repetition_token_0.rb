f4c9632801a2ae14fb3a82d094778fcf3c705169
Worof <worof.eng_5760@eng.kfs.edu.eg>
2024-01-30 17:07:17 -0800

    Repetition Token

---
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+
+# Check if an argument was provided
+if ARGV.length != 1
+  puts "Usage: ./script_name.rb <string>"
+  exit 1
+end
+
+# Get the input string
+input_string = ARGV[0]
+
+# Use a regular expression to match the pattern
+matches = input_string.scan(/hb+tt+n/)
+
+# Print the matches
+puts matches.join
