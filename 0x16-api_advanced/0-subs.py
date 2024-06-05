35caad24d3ca3456194445b3260ace133aebac35
Worof <worof.eng_5760@eng.kfs.edu.eg>
2024-06-05 13:04:32 -0700

    a function that queries the Reddit API and returns the number of subscribers (not active users, total subscribers) for a given subreddit.

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,10 +3,14 @@ import requests
 
 def number_of_subscribers(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "python:subreddit.subscriber.count:v1.0 (by /u/Automatic-River-2726)"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers)
     
-    if response.status_code == 200:
-        data = response.json()
-        return data["data"]["subscribers"]
+    if response.status_code != 200:
+        return 0
+    
+    data = response.json()
+    if 'data' in data and 'subscribers' in data['data']:
+        return data['data']['subscribers']
+    
     return 0
