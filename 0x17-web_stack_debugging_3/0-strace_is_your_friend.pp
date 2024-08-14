852ed882e32339db4d3ba8771d23ec2aad58f0b5
Worof <worof.eng_5760@eng.kfs.edu.eg>
2024-08-14 15:13:44 -0700

    Strace is your friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,17 +1,8 @@
 # 0-strace_is_your_friend.pp
-# Fix permissions on the WordPress directory
-file { '/var/www/html/wp-content':
-    ensure => 'directory',
-    owner  => 'www-data',
-    group  => 'www-data',
-    mode   => '0755',
-    recurse => true,
-    notify  => Service['apache2'],
-}
-
-# Ensure Apache is always running
-service { 'apache2':
-    ensure     => running,
-    enable     => true,
-    subscribe  => File['/var/www/html/wp-content'],
+# Fix file permissions for WordPress
+file { '/var/www/html/wp-config.php':
+  ensure => 'file',
+  owner  => 'www-data',
+  group  => 'www-data',
+  mode   => '0644',
 }
