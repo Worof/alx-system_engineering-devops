1bd422af311a12bf23affff5688a8d7d2fca7a7a
Worof <worof.eng_5760@eng.kfs.edu.eg>
2024-08-21 00:50:26 +0000

    a function that queries the Reddit API and returns the number of subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,25 +1,17 @@
 #!/usr/bin/python3
-"""
-Module: 0-subs
-This module contains the function `number_of_subscribers` that queries
-the Reddit API to get the number of subscribers for a given subreddit.
-"""
 import requests
 
 def number_of_subscribers(subreddit):
-    """
-    Queries the Reddit API and returns the number of subscribers for a given subreddit.
-    If the subreddit is invalid, return 0.
-    """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers)
+    """Returns the number of subscribers for a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Reddit-API-Query"}
     
-    if response.status_code != 200:
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            return data['data'].get('subscribers', 0)
+        else:
+            return 0
+    except requests.RequestException:
         return 0
-    
-    data = response.json()
-    if 'data' in data and 'subscribers' in data['data']:
-        return data['data']['subscribers']
-    
-    return 0
