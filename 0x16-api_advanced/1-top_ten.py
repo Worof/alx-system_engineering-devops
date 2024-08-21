bc45a2bad83e9cb8eeb91e18cfaf4043e5d0bcb8
Worof <worof.eng_5760@eng.kfs.edu.eg>
2024-08-21 00:53:53 +0000

    a function that queries the Reddit API and returns the number of subscribers

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,27 +1,21 @@
 #!/usr/bin/python3
-"""
-Module: 1-top_ten
-This module contains the function `top_ten` that queries the Reddit API
-to print the titles of the first 10 hot posts listed for a given subreddit.
-"""
 import requests
 
 def top_ten(subreddit):
-    """
-    Queries the Reddit API and prints the titles of the first 10 hot posts
-    listed for a given subreddit.
-    If not a valid subreddit, prints None.
-    """
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-
-    if response.status_code != 200:
-        print(None)
-        return
-
-    data = response.json()
-    posts = data.get('data', {}).get('children', [])
-
-    for post in posts:
-        print(post.get('data', {}).get('title', None))
+    """Prints the titles of the first 10 hot posts for a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    headers = {"User-Agent": "Reddit-API-Query"}
+    
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            posts = data.get('data', {}).get('children', [])
+            if not posts:
+                print("None")
+            for post in posts:
+                print(post['data'].get('title', "None"))
+        else:
+            print("None")
+    except requests.RequestException:
+        print("None")
